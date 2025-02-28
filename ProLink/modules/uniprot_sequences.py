b2dc71fdc4e346eacbb745cb4c60538fff9dc969
claugomezv <844321@unizar.es>
2025-02-28 09:40:39 +0100

    Adding xref_refseq to query fields

--- ProLink/modules/uniprot_sequences.py
+++ ProLink/modules/uniprot_sequences.py
@@ -20,7 +20,7 @@ def check_uniprot_batch(wp_codes):
     query = " OR ".join(f"xref_refseq:{wp}" for wp in wp_codes)
     params = {
         "query": query,
-        "fields": "accession",
+        "fields": "accession, xref_refseq", #Include xref_refseq to see the matches RefSeq IDs
         "format": "json",
         "size": len(wp_codes)  # Ensure we get as many results as the number of queries in the batch
     }
