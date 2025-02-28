86ead20cc9d9bf3d75082b390c946b9062423832
claugomezv <844321@unizar.es>
2025-02-28 09:56:01 +0100

    Erasing 3 latest commits

--- ProLink/modules/uniprot_sequences.py
+++ ProLink/modules/uniprot_sequences.py
@@ -20,7 +20,7 @@ def check_uniprot_batch(wp_codes):
     query = " OR ".join(f"xref_refseq:{wp}" for wp in wp_codes)
     params = {
         "query": query,
-        "fields": "accession, xref_refseq", #Include xref_refseq to see the matches RefSeq IDs
+        "fields": "accession", #Include xref_refseq to see the matches RefSeq IDs
         "format": "json",
         "size": len(wp_codes)  # Ensure we get as many results as the number of queries in the batch
     }
@@ -57,7 +57,7 @@ def filter_valid_sequences(input_fasta, output_fasta):
         if match:
             wp_data[seq.id] = match.group(1)
     
-    #print(f"Códigos WP extraídos: {list(wp_data.values())}")  # Debug: show extracted WP codes
+    print(f"Códigos WP extraídos: {list(wp_data.values())}")  # Debug: show extracted WP codes
     
     logger.info(f"Número total de secuencias: {len(sequences)}")
     logger.info(f"Número de códigos WP encontrados: {len(wp_data)}")
