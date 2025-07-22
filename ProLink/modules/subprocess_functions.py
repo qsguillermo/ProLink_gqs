f0e389142dc1221357f16d5e95a822d8dbd4b325
qsguillermo <gqsguillermo@gmail.com>
2025-07-22 12:44:59 +0200

    Update subprocess_functions.py

--- ProLink/modules/subprocess_functions.py
+++ ProLink/modules/subprocess_functions.py
@@ -35,7 +35,7 @@ def clean_label(label, protein_name=""):
 def clean_newick_string(newick_str, protein_name):
     if not protein_name:
         raise ValueError("❌ Se esperaba un nombre de proteína pero no ha llegado.")
-    print(f" [DEBUG] Nombre de proteína recibido clean_newick_string: {protein_name}")
+    print(f" [DEBUG] Protein name recibido clean_newick_string: {protein_name}")
     pattern = re.compile(
         r"('([^']+---C\d+[^']*)'|\"([^\"]+---C\d+[^\"]*)\"|([A-Za-z0-9 _\.\-]+---C\d+))",
         flags=re.IGNORECASE
